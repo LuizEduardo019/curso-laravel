874ea74b6b115ea585132de7338c1431d666956c
LuizEduardo019 <luizeduardodeoliveira166@gmail.com>
2022-05-27 15:52:56 -0300

    first commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -107,22 +107,38 @@ class EventController extends Controller
 
     }
 
-    public function update(Request $request)
+    public function update(Request $request) 
     {
         $data = $request->all();
 
-        $requestImage = $request->image;
+        if($request->hasFile('image') && $request->file('image')->isValid())
+        {
+            $requestImage = $request->image;
             
-        $extension = $requestImage->extension();
-        
-        $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
+            $extension = $requestImage->extension();
+            
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
 
-        $requestImage->move(public_path('img/events'), $imageName);
+            $requestImage->move(public_path('img/events'), $imageName);
+            
+            $data['image'] = $imageName;
 
-        $data['image'] = $imageName;
+        }
+
 
         Event::findOrFail($request->id)->update($data);
         return redirect('/dashboard')->with('msg','Evento editado com sucesso!');
+    }
+
+    public function joinEvent($id)
+    {
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Presença Confirmada');
     }
 
 }
